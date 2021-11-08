105c41e9a3c09a23148910ed6e76d8ab8c15b4ee
Christian <christian.nader@decathlon.com>
2021-11-08 15:41:34 +0100

    2 spec files :)

--- src/main/java/com/decathlon/poc/PocApplication.java
+++ src/main/java/com/decathlon/poc/PocApplication.java
@@ -1,13 +1,61 @@
 package com.decathlon.poc;
 
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.Serial;
 
 @SpringBootApplication
-public class PocApplication {
+@ComponentScan(basePackages = {"com.decathlon.poc", "com.decathlon.poc.petstore.api" , "com.decathlon.poc.petstore.config"})
+public class PocApplication implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... arg0) {
+		if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+			throw new PocApplication.ExitException();
+		}
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
+		new SpringApplication(PocApplication.class).run(args);
 	}
+
+	static class ExitException extends RuntimeException implements ExitCodeGenerator {
+		@Serial
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public int getExitCode() {
+			return 10;
+		}
+
+	}
+
+	@Bean
+	public WebMvcConfigurer webConfigurer() {
+		return new WebMvcConfigurer() {
+            /*@Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("Content-Type");
+            }*/
+		};
+	}
+
+	@Bean
+	public Module jsonNullableModule() {
+		return new JsonNullableModule();
+	}
+
 
 }
--- src/main/java/com/decathlon/poc/api/PetApiImpl.java
+++ src/main/java/com/decathlon/poc/api/PetApiImpl.java
@@ -1,6 +1,8 @@
 package com.decathlon.poc.api;
 
-import com.decathlon.poc.model.Pet;
+
+import com.decathlon.poc.stoplight.api.PetsApiDelegate;
+import com.decathlon.poc.stoplight.model.PetV1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +10,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class PetApiImpl implements PetApiDelegate{
+public class PetApiImpl implements PetsApiDelegate {
 
     @Override
-    public ResponseEntity<Pet> getPetById(Long petId) {
-log.info("azdazdadazdazdazdazdaz");
-        Pet pet = new Pet();
+    public ResponseEntity<PetV1> showPetById(String petId) {
+        PetV1 pet = new PetV1();
         pet.id(1L).name("rex");
+
         return new ResponseEntity<>(pet, HttpStatus.OK);
     }
 
