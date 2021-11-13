6f989fabed8f178d5680040511b2ae99aa65c42b
Christian <christian.nader@decathlon.com>
2021-11-13 12:44:44 +0100

    tests with schemathesis (update README later)

--- src/main/java/com/decathlon/poc/api/PetApiImpl.java
+++ src/main/java/com/decathlon/poc/api/PetApiImpl.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
 public class PetApiImpl implements PetsApiDelegate {
@@ -16,8 +19,20 @@ public class PetApiImpl implements PetsApiDelegate {
     public ResponseEntity<PetV1> showPetById(String petId) {
         PetV1 pet = new PetV1();
         pet.id(1L).name("rex");
-
         return new ResponseEntity<>(pet, HttpStatus.OK);
     }
 
+    @Override
+    public ResponseEntity<List<PetV1>> listPets(Integer limit) {
+        PetV1 rex = new PetV1();
+        rex.id(1L).name("rex");
+        PetV1 max = new PetV1();
+        max.id(1L).name("max");
+        return new ResponseEntity<>(new ArrayList<>(List.of(rex, max)), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> createPets() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
